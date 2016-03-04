4610a44c2387012d1f5ad0552aa676174c0cf38f
StuartSeupaul <stuartseupaul@gmail.com>
2016-03-03 19:51:59 -0500

    Added question and choice controller

--- Drivexus/db/schema.rb
+++ Drivexus/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160304003639) do
+ActiveRecord::Schema.define(version: 20160304004919) do
+
+  create_table "choices", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "question_id"
+    t.integer  "correct"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "cohorts", force: :cascade do |t|
     t.integer  "user_id"
