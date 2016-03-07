0a1e991697097cf4d9f47f2c3f13aa16434e97b0
StuartSeupaul <stuartseupaul@gmail.com>
2016-03-06 21:50:13 -0500

    Added style to list out correct answer, also refactored question index page slightly

--- Drivexus/app/controllers/attacheds_controller.rb
+++ Drivexus/app/controllers/attacheds_controller.rb
@@ -2,8 +2,15 @@ class AttachedsController < ApplicationController
   def create
     @attachedquestion = Attached.new(attached_params)
 
+    exID = attached_params[:exam_id]
+    @ex = Exam.find(exID)
+
     respond_to do |format|
       if @attachedquestion.save
+        @qs = Question.all
+        @attachedqs = @ex.questions
+        @index = @attachedqs.count - 1
+        @unusedqs = @qs - @attachedqs
         format.js
       end
     end
