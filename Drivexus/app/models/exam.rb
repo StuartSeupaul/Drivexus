61293a662c16e4597629d4826b08d6d8660738b5
StuartSeupaul <stuartseupaul@gmail.com>
2016-03-10 18:28:13 -0500

    Changing traffic lights working

--- Drivexus/app/models/exam.rb
+++ Drivexus/app/models/exam.rb
@@ -5,20 +5,25 @@ class Exam < ActiveRecord::Base
   has_many :scantrons
 
   def average_grade
-    total_score = 0
-    total_questions = 0
-    running_score = []
+    if self.scantron.any?
+      total_score = 0
+      total_questions = 0
+      running_score = []
 
-    self.scantrons.each do |scantron|
-      scantron.answers.each do |answer|
-        total_questions += 1
-        if answer.correct == true
-          total_score += 1
+      self.scantrons.each do |scantron|
+        if scantron.answers.any?
+          scantron.answers.each do |answer|
+            total_questions += 1
+            if answer.correct == true
+              total_score += 1
+            end
+          end
+        running_score << total_score/total_questions
         end
       end
-      running_score << total_score/total_questions
-    end
 
-    "#{((running_score.sum / running_score.length)*100)}%"
+      running_score != 0 ? "#{((running_score.sum / running_score.length)*100)}%" : ""
+    end
   end
+  
 end
