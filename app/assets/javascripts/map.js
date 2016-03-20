0ecff0fc69accc45c5cee53cf68595550f0f2d0b
sophiaxhe <sophiahxx@gmail.com>
2016-03-19 21:24:12 -0400

    set max zoom level

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,9 +2,11 @@ function initMap(){
   var mapPlaceHolder = $('#map');
   window.map = new google.maps.Map(mapPlaceHolder[0],{
     center: {lat: 43.653908, lng: -79.384293},
-    zoom: 12,
+    zoom: 10,
+    maxZoom: 13,
   fullscreenControl: true,
   });
+
 
 
 }
