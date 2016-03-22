905831ee53b73f550c47eac12c631712492bbb78
StuartSeupaul <stuartseupaul@gmail.com>
2016-03-22 14:03:48 -0400

    Added validations for some models

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -1,3 +1,8 @@
 class Choice < ActiveRecord::Base
+  validates :question_id, :presence => true
+  validates :content, :presence => true,
+                      :length => {minimum: 1},
+                      :length => {maximum: 200}
+
   belongs_to :question
 end
